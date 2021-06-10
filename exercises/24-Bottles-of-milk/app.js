f207817dac3ab7c6bbe8764adbac046b17052dd1
gars150387 <gar.santeliz@gmail.com>
2021-06-10 17:20:30 +0000

    origin

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,12 @@
-// Your code here:
+for (let i =0; i<99; i++){
+//   console.log(i)
+  if (i >1 && i<99){
+    console.log(`${i} bottles of milk on the wall, ${i} bottles of milk. Take one down and pass it around, ${i-1} bottles of milk on the wall.`)
+  }
+  else if (i>0 && i<2){
+    console.log(`${i} bottle of milk on the wall, ${i} bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.`)
+  }
+  else {
+  console.log('No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more.')
+}
+}
