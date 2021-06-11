c6741a0b900487325f1716f73be3d97a4e92f186
gars150387 <gar.santeliz@gmail.com>
2021-06-11 21:01:36 +0000

    origin

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,6 +1,6 @@
 function fizzBuzz() {  
     // Your code here
-for (let i=1; i<100; i++){
+for (let i=1; i<101; i++){
 //   console.log(i)
 if (i % 3 ===0 && i % 5 ===0){
   console.log('FizzBuzz');
