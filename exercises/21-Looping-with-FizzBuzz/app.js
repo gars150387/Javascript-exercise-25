adf12251b1941d63698ecb40c8c8b13c508e71fa
gars150387 <gar.santeliz@gmail.com>
2021-06-11 20:59:35 +0000

    origin

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,19 +1,19 @@
 function fizzBuzz() {  
     // Your code here
 for (let i=1; i<100; i++){
-  console.log(i)
+//   console.log(i)
 if (i % 3 ===0 && i % 5 ===0){
-  console.log('FizzBuzz')
+  console.log('FizzBuzz');
 }
   else if(i % 3 ===0){
-    console.log('Fizz')
+    console.log('Fizz');
   }
   else if (i % 5 ===0){
-    console.log('Buzz')
+    console.log('Buzz');
   }
   else{
-    console.log(i)
+    console.log(i);
   }
 }
 }
-console.log(fizzBuzz())
+fizzBuzz()
